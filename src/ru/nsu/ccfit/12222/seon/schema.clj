0c279aa8751c6175962a8907e3aa915b6c38dde0
zhirov <zhirov@5a6aabce-a326-4528-a5e9-8409bbc25adf>
2013-12-18 12:16:44 +0000

    ADD: Added method for type-independent property validation
    git-svn-id: https://ccfit.nsu.ru/svn/DT6/2013/group-12222/seon/trunk@357 5a6aabce-a326-4528-a5e9-8409bbc25adf

--- src/ru/nsu/ccfit/12222/seon/schema.clj
+++ src/ru/nsu/ccfit/12222/seon/schema.clj
@@ -1,10 +1,17 @@
 (ns ru.nsu.ccfit.12222.seon.schema
   (:use [ru.nsu.ccfit.12222.seon.core]))
 
-(defmulti valid?
+(defmulti valid?-type
           "Validate SEON expression against schema."
           (fn [schema expr]
             (:type schema)))
+
+(defn valid?
+  "Validate SEON expression common for all types."
+  [schema expr]
+  (and
+    (if (:type schema) (valid?-type schema expr) true))
+  )
 
 (defn invalid?
   "Shortcut for (not (valid schema expr))."
@@ -12,15 +19,15 @@
   (not (valid? schema expr)))
 
 
-(defmethod valid? :default
+(defmethod valid?-type :default
   [schema expr]
   (throw (Exception. (str "Unknown schema type: " (serialize schema)))))
 
-(defmethod valid? "integer"
+(defmethod valid?-type "integer"
   [schema expr]
   (integer? expr))
 
-(defmethod valid? "number"
+(defmethod valid?-type "number"
   [schema expr]
   (or (float? expr) (integer? expr)))
 
@@ -28,38 +35,38 @@
   [schema expr]
   (nil? expr))
 
-(defmethod valid? "null"
+(defmethod valid?-type "null"
            [schema expr]
   (valid?-nil schema expr))
 
-(defmethod valid? "nil"
+(defmethod valid?-type "nil"
            [schema expr]
   (valid?-nil schema expr))
 
-(defmethod valid? "string"
+(defmethod valid?-type "string"
            [schema expr]
   (string? expr))
 
-(defmethod valid? "boolean"
+(defmethod valid?-type "boolean"
            [schema expr]
   (or (true? expr) (false? expr)))
 
-(defmethod valid? "object"
+(defmethod valid?-type "object"
   [schema expr]
   (and
     (map? expr)
     (if (:properties schema)
       (every? true? (map (fn [[property schema]]
                            (if (or (:required schema) (property expr))
-                             (valid? schema (property expr))
+                             (valid?-type schema (property expr))
                              true)
                            ) (:properties schema)))
       true)))
 
-(defmethod valid? "array"
+(defmethod valid?-type "array"
            [schema expr]
   (and
     (seq? expr)
     (if (:items schema)
-      (every? true? (map (partial valid? (:items schema)) expr))
+      (every? true? (map (partial valid?-type (:items schema)) expr))
       true)))
