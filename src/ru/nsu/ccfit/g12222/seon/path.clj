883865091333889f89e0295fc271e8f82c5634ea
Nevkontakte <aleks@nevkontakte.com>
2018-09-22 23:50:22 +0100

    Some god forsaken fix which I never committed before.

--- src/ru/nsu/ccfit/g12222/seon/path.clj
+++ src/ru/nsu/ccfit/g12222/seon/path.clj
@@ -1,38 +1,74 @@
 (ns ru.nsu.ccfit.g12222.seon.path
   (:use [ru.nsu.ccfit.g12222.seon.core]))
 
-(defn function
-  [expr mas]
+
+(defn walk-expression
+  [expr mas stack]
   (if (nil? mas)
+
     (list expr)
-    (cond (seon-map? expr)
-          (cond (= "*" (first mas))
-                (map
-                  (fn [subExp] (function subExp (next mas)))
-                  (vals expr))
+    (cond
+      (= (first mas) "{") (if (= (second mas) expr) (list expr) `())
 
-                (= ".." (first mas))
-                (concat
-                  (function expr (next mas))
-                  (reduce concat (map (fn [subExp]
-                                        (function subExp mas))
-                                      (vals expr)))
-                  )
+      (seon-map? expr)
+      (cond (= "*" (first mas))
+            (reduce concat (map
+                             (fn [subExp] (walk-expression subExp (next mas) stack))
+                             (vals expr)))
 
 
 
-                :else
-                (function ((keyword (first mas)) expr) (next mas)))
-          :else (list expr)
-          ))
+            (= ".." (first mas))
+            (concat
+              (walk-expression expr (next mas) stack)
+              (reduce concat (map (fn [subExp]
+                                    (walk-expression subExp mas stack))
+                                  (vals expr)))
+              )
+
+
+
+
+
+            :else
+            (walk-expression ((keyword (first mas)) expr) (next mas) stack))
+
+      (seon-list? expr)
+      (cond (= ".." (first mas))
+            (reduce concat (map
+                             (fn [subExp] (walk-expression subExp (next mas)))
+                             expr))
+
+            (= "*" (first mas))
+            (concat
+              (walk-expression expr (next mas))
+              (reduce concat (map (fn [subExp]
+                                    (walk-expression subExp mas))
+                                  expr))
+              )
+
+            :else
+            (let [index (Integer/parseInt (first mas))]
+              (cond
+                (>= index (count expr)) `()
+                :else (walk-expression (nth expr index) (next mas) stack)
+                )))
+
+
+      (and (not= mas nil) (seon-atom? expr)) `()
+      :else (list expr)
+      ))
 
   )
 
-
-(defn parser
+(defn query
   [expr string]
   {:pre [(seon? expr)]}
-  (function expr (re-seq #"\w+|[.]{2}|(?<=\[)[0-9]+(?=\])" string))
+  (walk-expression expr (re-seq #"\w+|[.]{2}|(?<=\[)[0-9]+(?=\])|[\*]|[\{]+|[\w+]+(?=\})" string) ())
 
   )
+
+
+
+
 
--- test/ru/nsu/ccfit/g12222/seon/test/path_test.clj
+++ test/ru/nsu/ccfit/g12222/seon/test/path_test.clj
@@ -1,14 +1,29 @@
 (ns ru.nsu.ccfit.g12222.seon.test.path-test
-  (:use [ru.nsu.ccfit.g12222.seon.core])
   (:use [ru.nsu.ccfit.g12222.seon.path])
-  (:use [ru.nsu.ccfit.g12222.seon.schema])
+  (:use [ru.nsu.ccfit.g12222.seon.core])
+  
   (:use [clojure.test]))
 
-(deftest parser-test
+
+(deftest parser-test1
   (testing "Have fun."
 
-    (is (= `({:b "abc", :t {:k {:f "gdk"}}, :u {:d 2, :e 3}} "abc" {:k {:f "gdk"}} {:f "gdk"} "gdk" {:d 2, :e 3} 2 3) (parser  `{:b "abc" :u {:d 2 :e 3} :t {:k {:f "gdk"}}} "..")))
+    (is (= `({:b "abc", :t {:k {:f "gdk"}}, :u {:d 2, :e 3}} {:k {:f "gdk"}} {:f "gdk"} {:d 2, :e 3})
+           (query  `{:b "abc" :u {:d 2 :e 3} :t {:k {:f "gdk"}}} "..")))
 
     ))
 
+(deftest parser-test2
+  (testing "Have fun2."
+    (is (= `("frt") (query  `{:u {:f {:t "frt"}} :r {:e "abc", :x "cde"}} "$*.*.t") ))
+    ))
 
+(deftest parser-test2
+  (testing "Have fun5."
+    (is (= `("abc") (query  `{:a {:b "cde" :c "abc"  :d ()} :b {:k 1}} "*.*{abc}") ))
+    ))
+
+(deftest parser-test2
+  (testing "Have fun6."
+    (is (= `("cde") (query  `("abc" "cde") "1") ))
+    ))
